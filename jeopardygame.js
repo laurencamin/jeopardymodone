da9ff0423aae5e15981e35610714d41803dfae73
Lauren <laurenc2335@gmail.com>
2023-03-20 14:10:18 -0500

    Commit MOJG

---
+++ jeopardygame.js
@@ -0,0 +1,35 @@
+//Welcome to Two Player Jeopardy!
+
+
+
+
+//To pick a theme, Player One and Player Two must flip a coin
+ 
+//The player that wins picks the first theme.
+
+//The game has four book themes to choose from.
+
+//After the player has chosen the theme, they can pick the level of difficulty of questions for the round.
+
+//Set the timer for each round.
+function setTimer() {
+
+}
+//To answer the question, Player One and Player Two will click on their assigned buttons.
+function playerOne() {
+
+
+
+
+
+
+
+
+
+
+}
+
+//When the round is over, Player One or Player Two will see their scores on the screen.
+
+//The player who has the highest score after all four rounds are completed will win the game.
+
