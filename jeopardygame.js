99191ac6fce8025a189bee576128c26c3954b5c8
Lauren <laurenc2335@gmail.com>
2023-03-21 15:27:00 -0500

    Commit 4

--- jeopardygame.js
+++ jeopardygame.js
@@ -1,25 +1,41 @@
 //Welcome to Two Player Jeopardy!
-function playerOne()
-      alert("Player One get ready")
-function playerTwo()
-      alert("Player Two get ready")
+//function playerOne()
+      
+//function playerTwo()
+      
 
-function startGame() {
+
+alert("Welcome to Two Player Jeopardy!")
 
 //To pick a theme, Player One and Player Two must flip a coin
+window.prompt("Flip a coin! The winner will pick the first theme and difffiulty of the questions. Type flip to do so.");
 function coinFlip() {
     return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
 }
 if (coinFlip === "heads") {
+    alert("Player One picks the theme!")
+    } else {
+    alert("Player Two picks the theme!")
+    }
+
+/*if (coinFlip === "heads") {
     playerOne();
 } else {
-   playerTwo();
-}
-}
+   playerTwo();*/
+//}
+
 //The player that wins the coin toss picks the first theme.
 //The game has four book themes to choose from.
 window.prompt("Pick a theme by typing 1, 2, 3, or 4.")
+let themeOne = 1
+let themeTwo = 2
+let themeThree = 3
+let themeFour= 4
 
+if (window.prompt === themeOne) {
+           startGame();
+}
+ 
 //After the player has chosen the theme, they can pick the level of difficulty of questions for the round.
 //The player uses a slider to pick the difficulty for the round.
 var slider = document.getElementById("difficultySlider");
@@ -40,7 +56,13 @@ function playerOne() {*/
 {}
 
 //When the round is over, Player One or Player Two will see their scores on the screen.
+alert("Time's Up!")
+alert("Player One Score:")
+alert("Player Two Score:")
 
 //The player who has the highest score after all four rounds are completed will win the game.
-{
-};
+winGame() 
+    alert("Congratulations! Player won the game!");
+loseGame() 
+    alert("Sorry, you didn't win this time!");
+    
